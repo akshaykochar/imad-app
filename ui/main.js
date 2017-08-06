7120f355fd80b83a59bb300109a5af063787e45b
akshaykochar <akochar@purdue.edu>
2017-08-06 18:59:55 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+
+var element = document.getElementById("main-text");
+element.innerHtml = 'This is Awesome!\n\nAWESOME';
+
+var img = document.getElementById('modi');
+var margin_left = 0 ;
+function moveRight(){
+    margin_left = margin_left + 1;
+    img.style.marginLeft = margin_left + 'px' ;
+}
+img.onclick = function(){
+    var interval = setInterval(moveRight, 15);
+};
